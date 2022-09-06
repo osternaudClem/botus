2185a1cb6e552eb27b98a6d0a8da82d7ad018f9f
Clement Osternaud <osternaud.clement@gmail.com>
2022-09-06 16:54:03 +0200

    Init

---
+++ index.js
@@ -0,0 +1,74 @@
+const { Client, GatewayIntentBits, EmbedBuilder  } = require('discord.js');
+const TOKEN = 'MTAxNjY5OTE3ODA2MTEzOTk2OA.GPGkN1.6nkv0EatLxAuj2nN3vmcw3MFBxX4n1aVM7bd5U';
+const PREFIX = '!';
+const REGEX_DICE = new RegExp(/(\d*)(D)(\d*)?/i);
+const colors = {
+  succes: 0x3DE232,
+  failed: 0xE23236,
+  default: 0x3281E2,
+}
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('>>> Bot ready');
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(PREFIX)) {
+    const msg = message.content.substring(1);
+
+    message.member.displayName
+
+    if (!REGEX_DICE.test(msg)) return;
+
+    let results = [];
+
+    console.log('>>> Roll dice')
+    const cmd = msg.split(REGEX_DICE);
+
+    const dice_number = cmd[1] || 1;
+    const dice_limit = cmd[3] || 100;
+
+    for (let i = 0; i < dice_number; i++) {
+      results.push(randomize(dice_limit));
+    }
+
+    let color = colors.default;
+
+    if (dice_number === 1 && dice_limit === 100) {
+      if (results[0] <= 5) {
+        color = colors.succes;
+      }
+      else if (results[0] >= 95) {
+        color = colors.failed;
+      }
+    }
+
+    const embed = new EmbedBuilder()
+      .setColor(color)
+      .setDescription(`**${message.member.displayName}** ${message.author.username === 'PhoRésie' ? ' (PS: Love you) ' : ''} a lancé ${dice_number} dé${dice_number > 1 ? 's' : ''} de ${dice_limit}`)
+      .addFields(
+        { name: 'Résultats ', value: `${results.map(result => { return `[${result}]` })}` },
+      )
+
+      message.channel.send({ embeds: [embed]});
+  }
+
+});
+
+client.login(TOKEN);
+
+function randomize(limit) {
+  min = Math.ceil(1);
+  max = Math.floor(limit);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
