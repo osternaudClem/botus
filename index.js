2dbfee2a976f96f91acde6abcc5c004d40a91409
Clement Osternaud <osternaud.clement@gmail.com>
2023-01-24 16:48:30 +0100

    Add env variable

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const PREFIX = '!';
 const REGEX_DICE = new RegExp(/(\d*)(D)(\d*)?/i);
 const colors = {
-  succes: 0x3DE232,
-  failed: 0xE23236,
-  default: 0x3281E2,
-}
+  succes: 0x3de232,
+  failed: 0xe23236,
+  default: 0x3281e2,
+};
 
 const client = new Client({
   intents: [
@@ -20,19 +23,19 @@ client.on('ready', () => {
   console.log('>>> Bot ready');
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', (message) => {
   if (message.author.bot) return;
 
   if (message.content.startsWith(PREFIX)) {
     const msg = message.content.substring(1);
 
-    message.member.displayName
+    message.member.displayName;
 
     if (!REGEX_DICE.test(msg)) return;
 
     let results = [];
 
-    console.log('>>> Roll dice')
+    console.log('>>> Roll dice');
     const cmd = msg.split(REGEX_DICE);
 
     const dice_number = cmd[1] || 1;
@@ -47,26 +50,31 @@ client.on('messageCreate', message => {
     if (dice_number === 1 && dice_limit === 100) {
       if (results[0] <= 5) {
         color = colors.succes;
-      }
-      else if (results[0] >= 95) {
+      } else if (results[0] >= 95) {
         color = colors.failed;
       }
     }
 
     const embed = new EmbedBuilder()
       .setColor(color)
-      .setDescription(`**${message.member.displayName}** a lancé ${dice_number} dé${dice_number > 1 ? 's' : ''} de ${dice_limit}`)
-      .addFields(
-        { name: 'Résultats ', value: `${results.map(result => { return `[${result}]` })}` },
-      );
+      .setDescription(
+        `**${message.member.displayName}** a lancé ${dice_number} dé${
+          dice_number > 1 ? 's' : ''
+        } de ${dice_limit}`
+      )
+      .addFields({
+        name: 'Résultats ',
+        value: `${results.map((result) => {
+          return `[${result}]`;
+        })}`,
+      });
 
     if (message.author.username === 'PhoRésie') {
-      embed.setFooter({ text: '(PS: Love you White !)' })
+      embed.setFooter({ text: '(PS: Love you White !)' });
     }
 
     message.channel.send({ embeds: [embed] });
   }
-
 });
 
 client.login(process.env.DISCORD_TOKEN);
