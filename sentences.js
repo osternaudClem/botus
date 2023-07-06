af0c71365b984df4469eb281bf3823497ec07fe7
Clement Osternaud <osternaud.clement@gmail.com>
2023-07-06 12:23:35 +0200

    fix(Sentences): renmove some sentences

--- sentences.js
+++ sentences.js
@@ -2,6 +2,4 @@ exports.sentences = [
   ['Oui, je suis ton createur petit Botus'],
   ['(PS: Love you White !)'],
   ['Bravo Maître du jeu !! Vous êtes le meilleur !'],
-  ['Alors, je suis morte ???'],
-  ['Alors, je suis morte ???'],
 ];
